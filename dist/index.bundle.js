7263b59af6f1a342cc91860dcbfd0c8e02659a89
Fernando Calderon <ffercc@gmail.com>
2021-11-08 14:04:21 +0100

    First commit

---
+++ dist/index.bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+
+
+//import * as initpage from "./initpage.js"
+
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFZOztBQUVaIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VicGFjay1kZW1vLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiXG5cbi8vaW1wb3J0ICogYXMgaW5pdHBhZ2UgZnJvbSBcIi4vaW5pdHBhZ2UuanNcIlxuXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ src/index.js
@@ -0,0 +1,4 @@
+"use strict"
+
+//import * as initpage from "./initpage.js"
+
